fd03ec1c0c8dc379382e5a65b71c531d5afa507c
Vladimir Parfenov <vparfenov@ptc.com>
2019-08-22 12:51:39 -0400

    Checking KeyVault module features types

--- session.js
+++ session.js
@@ -149,7 +149,7 @@ class SessionDB {
   getMSITokenCredentials () {
     return `${KeyVault.MSITokenCredentials}`
   }
-  getKeyVaultExports () {return Object.keys (KeyVault)} 
+  getKeyVaultExports () {return Object.keys (KeyVault).map (name => `${name}: ${typeof KeyValue [name]}`)} 
   getCredentials () {return getKeyValutCredentials ()} 
   getKeyVault () {return getKeyVaultClient ()} 
   getSecret (name) {
